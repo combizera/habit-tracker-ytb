4e40b0d057465854956ece6961b281072f2329af
combizera <ycombi349@gmail.com>
2025-11-09 17:15:59 -0300

    feat: create logout route and improve logic

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -9,9 +9,11 @@ class SiteController extends Controller
         $name = 'Fulano';
         $habits = ['Ler', 'Correr', 'Estudar', 'Viajar'];
 
-        return view('home', [
-            'name' => $name,
-            'habits' => $habits
-        ]);
+        return view('home', compact('name', 'habits'));
+    }
+
+    public function dashboard()
+    {
+        return view('dashboard');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,12 @@ use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Route;
 
-// SITE
-Route::get('/', [SiteController::class, 'index']);
+Route::get('/', [SiteController::class, 'index'])->name('site.index');
+Route::get('/login', [LoginController::class, 'index'])->name('site.login');
+Route::post('/login', [LoginController::class, 'authenticate'])->name('auth.login');
 
-// LOGIN
-Route::get('/login', [LoginController::class, 'index']);
-Route::post('/login', [LoginController::class, 'authenticate']);
+// AUTH
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', [SiteController::class, 'dashboard'])->name('site.dashboard');
+    Route::post('/logout', [LoginController::class, 'logout'])->name('auth.logout');
+});
