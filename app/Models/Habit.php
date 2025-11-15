e99d24e2b7cf6b143d0534be107d116e7557d19f
combizera <ycombi349@gmail.com>
2025-11-15 16:55:49 -0300

    feat: create relationships between Habit/HabitLog/User

--- app/Models/Habit.php
+++ app/Models/Habit.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Habit extends Model
 {
@@ -10,4 +12,16 @@ class Habit extends Model
         'user_id',
         'name',
     ];
+
+    // Um hábito pertence a um usuário
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // Um hábito pode ter muitos registros
+    public function habitLogs(): HasMany
+    {
+        return $this->hasMany(HabitLog::class);
+    }
 }
