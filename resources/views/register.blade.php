dcdb27baf483c68b12d377b0be383f63e022f94f
combizera <ycombi349@gmail.com>
2025-11-14 23:26:14 -0300

    feat(register): create view/controller/request

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -3,15 +3,31 @@
      <section class="bg-white max-w-[600px] mx-auto p-10 pb-6 border-2 mt-4">
 
        <h1 class="font-bold text-3xl">
-         Faça Login
+         Registre-se
        </h1>
 
        <p>
-         Insira seus dados para acessar
+         Preencha as informações para se cadastrar seus hábitos
        </p>
 
-      <form action="{{ route('auth.login') }}" method="POST" class="flex flex-col">
+      <form action="{{ route('auth.register') }}" method="POST" class="flex flex-col">
         @csrf
+        <div class="flex flex-col gap-2 mb-2">
+          <label for="name">
+            Nome
+          </label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Seu nome"
+            class="bg-white p-2 border-2 @error('name') border-red-500 @enderror"
+          >
+          @error('name')
+          <p class="text-red-500 text-sm">
+            {{ $message }}
+          </p>
+          @enderror
+        </div>
 
         <div class="flex flex-col gap-2 mb-2">
           <label for="email">
@@ -49,18 +65,37 @@
           @enderror
         </div>
 
+        <div class="flex flex-col gap-2 mb-4">
+          <label for="password_confirmation">
+            Repita sua senha
+          </label>
+
+          <input
+            type="password"
+            name="password_confirmation"
+            placeholder="********"
+            class="bg-white p-2 border-2 @error('password') border-red-500 @enderror"
+          >
+
+          @error('password')
+            <p class="text-red-500 text-sm">
+              {{ $message }}
+            </p>
+          @enderror
+        </div>
+
         <button
           type="submit"
           class="bg-white border-2 p-2"
         >
-          Entrar
+          Cadastrar
         </button>
       </form>
 
        <p class="text-center mt-4">
-         Ainda não tem uma conta?
-          <a href="{{ route('site.register') }}" class="underline hover:opacity-50 transition">
-            Registre-se
+         Já tem uma conta?
+          <a href="{{ route('site.login') }}" class="underline hover:opacity-50 transition">
+            Faça login
           </a>
        </p>
     </section>
