6439f969f827ae992d1e8296ee873d4324363b85
combizera <ycombi349@gmail.com>
2025-12-23 08:47:51 -0300

    feat: add habit history view

--- resources/views/components/contribution.blade.php
+++ resources/views/components/contribution.blade.php
@@ -51,14 +51,17 @@
               <div class="w-3 h-3"></div>
             @else
               @php
-                // TODO: Verificar se tem log nessa data
-                // Por enquanto randômico
-                $done = rand(0, 1);
+                $hasDone = $habit->habitLogs
+                    ->where('completed_at', $day->toDateString())
+                    ->isNotEmpty();
               @endphp
-              <div class="w-3 h-3 rounded-xs cursor-pointer transition hover:ring-2 hover:ring-blue-400
-                       {{ $done ? 'bg-[#FF7A05]' : 'bg-[#DADFE9]' }}"
-                   title="{{ $day->format('d/m/Y') }} - {{ $day->translatedFormat('l') }}"
-              ></div>
+              <div
+                class="w-3 h-3 rounded-xs cursor-pointer transition hover:ring-2 hover:ring-blue-400
+                       {{ $hasDone ? 'bg-[#FF7A05]' : 'bg-[#DADFE9]' }}"
+                title="{{ $day->format('d/m/Y') }} - {{ $day->translatedFormat('l') }}"
+              >
+
+              </div>
             @endif
           @endforeach
         </div>
